d993af3880e9e14e321849906b7e51ec4f98824a
Tony Farrelly <tfarrelly01@gmail.com>
2022-08-02 17:37:01 +0100

    Update item quantity, Connect API, mapDispatchToProps

--- src/store/items/reducer.js
+++ src/store/items/reducer.js
@@ -1,4 +1,4 @@
-import { ITEM_ADDED, ITEM_REMOVED, ITEM_PRICE_UPDATED } from './actions';
+import { ITEM_ADDED, ITEM_REMOVED, ITEM_PRICE_UPDATED, ITEM_QUANTITY_UPDATED } from './actions';
 
 let id = 1;
 
@@ -21,6 +21,15 @@ export const reducer = (state = initialItems, action) => {
     return state.map(item => {
       if (item.uuid === action.payload.uuid) {
         return { ...item, price: action.payload.price };
+      }
+      return item;
+    });
+  }
+
+  if (action.type === ITEM_QUANTITY_UPDATED) {
+    return state.map(item => {
+      if (item.uuid === action.payload.uuid) {
+        return { ...item, quantity: action.payload.quantity };
       }
       return item;
     });
