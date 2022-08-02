4bd8cd7eda0449e14f85a2e3da37c40377f01e96
Tony Farrelly <tfarrelly01@gmail.com>
2022-08-02 16:17:45 +0100

    mapDispatchToProps

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,20 +1,17 @@
 import { Card } from '@twilio-paste/core';
 import { TipSelectContainer } from '../containers/TipSelectContainer';
 
-import { MenuItems } from './MenuItems';
+import { MenuItemsContainer } from '../containers/MenuItemsContainer';
+// import { MenuItems } from './MenuItems';
 import { NewItemForm } from './NewItemForm';
 import { Summary } from './Summary';
-
-const items = [
-  { uuid: 1, name: 'Tofu Roast', price: 14, quantity: 1 },
-  { uuid: 2, name: 'Vegan Ham', price: 12, quantity: 1 }
-];
 
 const Calculator = () => {
   return (
     <Card>
       <NewItemForm />
-      <MenuItems items={items} />
+      <MenuItemsContainer />
+      {/*<MenuItems items={items} />*/}
       <TipSelectContainer />
       <Summary />
     </Card>
