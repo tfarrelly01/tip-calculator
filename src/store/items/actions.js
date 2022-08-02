6e54d84a6b3434b41740a485f6355a984494a555
Tony Farrelly <tfarrelly01@gmail.com>
2022-08-02 17:13:42 +0100

    removeItem - Connect API and mapDispatchToProps

--- src/store/items/actions.js
+++ src/store/items/actions.js
@@ -1,4 +1,5 @@
 export const ITEM_ADDED = 'ITEM_ADDED';
+export const ITEM_REMOVED = 'ITEM_REMOVED';
 
 export const addNewItem = (name, price) => ({
     type: ITEM_ADDED,
@@ -7,3 +8,12 @@ export const addNewItem = (name, price) => ({
         price
     }
 });
+
+export const removeItem = (uuid) => {
+    return {
+        type: ITEM_REMOVED,
+        payload: {
+            uuid
+        }
+    };
+};
--- src/store/items/reducer.js
+++ src/store/items/reducer.js
@@ -1,4 +1,4 @@
-import { ITEM_ADDED } from './actions';
+import { ITEM_ADDED, ITEM_REMOVED } from './actions';
 
 let id = 1;
 
@@ -11,6 +11,10 @@ export const reducer = (state = initialItems, action) => {
   if (action.type === ITEM_ADDED) {
     const item = { uuid: id++, quantity: 1, ...action.payload };
     return [...state, item];
+  }
+
+  if (action.type === ITEM_REMOVED) {
+    return state.filter(item => item.uuid !== action.payload.uuid);
   }
 
   return state;
