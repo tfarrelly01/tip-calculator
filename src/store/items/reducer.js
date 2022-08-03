4dff72c6cc10b2a3fb2fa1c93b8345b6b4266335
Tony Farrelly <tfarrelly01@gmail.com>
2022-08-03 13:42:15 +0100

    Wrap Immer around reducer

--- src/store/items/reducer.js
+++ src/store/items/reducer.js
@@ -8,66 +8,27 @@ export const initialItems = [
   { uuid: id++, name: 'Vegan Ham Sandwich', price: 12, quantity: 1 }
 ];
 
-export const reducer = (state = initialItems, action) => {
+// Wrap Immer around the reducer
+export const reducer = produce((state = initialItems, action) => {
 
-  // Using Immer
   if (action.type === ITEM_ADDED) {
-    return produce(state, (draftState) => {
       const item = { uuid: id++, quantity: 1, ...action.payload };
-      draftState.push(item);
-    })
+      state.push(item);
   }
-
-  /*
-  if (action.type === ITEM_ADDED) {
-    const item = { uuid: id++, quantity: 1, ...action.payload };
-    return [...state, item];
-  }
-  */
 
   if (action.type === ITEM_REMOVED) {
     return state.filter(item => item.uuid !== action.payload.uuid);
   }
 
-  // Using Immer
   if (action.type === ITEM_PRICE_UPDATED) {
-    return produce(state, (draftState) => {
-      const item = draftState.find((item) => item.uuid === action.payload.uuid);
+      const item = state.find((item) => item.uuid === action.payload.uuid);
       item.price = parseInt(action.payload.price, 10);
-    });
   }
-/*
-  /*
-  if (action.type === ITEM_PRICE_UPDATED) {
-    return state.map(item => {
-      if (item.uuid === action.payload.uuid) {
-        return { ...item, price: action.payload.price };
-      }
-      return item;
-    });
-  }
-  */
 
-  // Using Immer
   if (action.type === ITEM_QUANTITY_UPDATED) {
-    return produce(state, (draftState) => {
-      const item = draftState.find((item) => item.uuid === action.payload.uuid);
+      const item = state.find((item) => item.uuid === action.payload.uuid);
       item.quantity = parseInt(action.payload.quantity, 10);
-    });
   }
-
-  /*
-  if (action.type === ITEM_QUANTITY_UPDATED) {
-    return state.map(item => {
-      if (item.uuid === action.payload.uuid) {
-        return { ...item, quantity: action.payload.quantity };
-      }
-      return item;
-    });
-  }
-  */
-
-  return state;
-};
+}, initialItems);
 
 export default reducer;
