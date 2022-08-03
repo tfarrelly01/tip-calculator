897cd10dbaa919f197ee5213bde432519953f2ec
Tony Farrelly <tfarrelly01@gmail.com>
2022-08-03 10:29:48 +0100

    Use Immer for individual action types

--- src/store/items/reducer.js
+++ src/store/items/reducer.js
@@ -1,3 +1,4 @@
+import produce from 'immer';
 import { ITEM_ADDED, ITEM_REMOVED, ITEM_PRICE_UPDATED, ITEM_QUANTITY_UPDATED } from './actions';
 
 let id = 1;
@@ -8,15 +9,35 @@ export const initialItems = [
 ];
 
 export const reducer = (state = initialItems, action) => {
+
+  // Using Immer
+  if (action.type === ITEM_ADDED) {
+    return produce(state, (draftState) => {
+      const item = { uuid: id++, quantity: 1, ...action.payload };
+      draftState.push(item);
+    })
+  }
+
+  /*
   if (action.type === ITEM_ADDED) {
     const item = { uuid: id++, quantity: 1, ...action.payload };
     return [...state, item];
   }
+  */
 
   if (action.type === ITEM_REMOVED) {
     return state.filter(item => item.uuid !== action.payload.uuid);
   }
 
+  // Using Immer
+  if (action.type === ITEM_PRICE_UPDATED) {
+    return produce(state, (draftState) => {
+      const item = draftState.find((item) => item.uuid === action.payload.uuid);
+      item.price = parseInt(action.payload.price, 10);
+    });
+  }
+/*
+  /*
   if (action.type === ITEM_PRICE_UPDATED) {
     return state.map(item => {
       if (item.uuid === action.payload.uuid) {
@@ -25,7 +46,17 @@ export const reducer = (state = initialItems, action) => {
       return item;
     });
   }
+  */
 
+  // Using Immer
+  if (action.type === ITEM_QUANTITY_UPDATED) {
+    return produce(state, (draftState) => {
+      const item = draftState.find((item) => item.uuid === action.payload.uuid);
+      item.quantity = parseInt(action.payload.quantity, 10);
+    });
+  }
+
+  /*
   if (action.type === ITEM_QUANTITY_UPDATED) {
     return state.map(item => {
       if (item.uuid === action.payload.uuid) {
@@ -34,6 +65,7 @@ export const reducer = (state = initialItems, action) => {
       return item;
     });
   }
+  */
 
   return state;
 };
